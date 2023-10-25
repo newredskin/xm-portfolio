5ab0174ae8bb0107ac941b4ac541a46022ee0a3b
newredskin <xiyemou@gmail.com>
2023-10-25 12:54:33 -0500

    setup color palette, add navbar

--- xiye-portfolio/src/App.jsx
+++ xiye-portfolio/src/App.jsx
@@ -1,5 +1,26 @@
+// third-party library imports
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// local imports
+import AppLayout from "./ui/AppLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, Xiye</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ xiye-portfolio/src/ui/NavBar.jsx
@@ -0,0 +1,94 @@
+import { NavLink } from "react-router-dom";
+import {
+  Bars3CenterLeftIcon,
+  CodeBracketSquareIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+function NavBar() {
+  const links = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/contact" },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="bg-teal-950">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4 focus:outline-none  ">
+        <div className="flex items-center gap-2 ">
+          <NavLink to="/">
+            <CodeBracketSquareIcon className="w-10 h-10 text-red-700" />
+          </NavLink>
+          <span className="text-stone-200">XIYE MOU</span>
+        </div>
+        <button
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-stone-200 rounded-lg sm:hidden hover:bg-gray-100/20 focus:outline-none focus:ring-2 focus:ring-gray-2"
+          onClick={() => setIsOpen((o) => !o)}
+        >
+          {isOpen ? <XMarkIcon /> : <Bars3CenterLeftIcon />}
+        </button>
+
+        <div
+          className={`w-full sm:block sm:w-auto ${isOpen ? "flex" : "hidden"}`}
+          id="navbar-default"
+        >
+          <ul className={"sm:flex sm:gap-8"}>
+            {links.map((link) => (
+              <li className="text-xl m-2 text-stone-200" key={link.name}>
+                <NavLink to={link.path}>{link.name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
+
+/*
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+*/
