e2102f10e7eb7d71a377978970eb06c307e48745
newredskin <xiyemou@gmail.com>
2023-10-31 20:19:08 -0500

    change order of projects

--- src/features/projects/projectData.js
+++ src/features/projects/projectData.js
@@ -3,28 +3,47 @@ const supabaseURL =
 
 const projectData = [
   {
-    name: "aavs-workshop",
-    category: "research-visualization",
-    title: "Growth",
+    name: "designcamp",
+    category: "web-dev",
+    title: "Design Camp",
     introduction:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [
-      supabaseURL + "aavs-workshop/" + "aavs_built.webp",
-      supabaseURL + "aavs-workshop/" + "aavs_photo-1.webp",
-    ],
-    tools: ["processing", "keyshot", "rhino", "grasshopper", "VR/AR"],
+    images: [supabaseURL + "designcamp/" + "designcamp_cover.webp"],
+    tools: ["javascript", "html&css", "express", "mongodb", "cloudinary"],
   },
   {
-    name: "nlsd",
-    category: "landscape",
-    title: "North Lake Shore Drive",
+    name: "spas",
+    category: "web-dev",
+    title: "Practice - SPA (single-page apps)",
     introduction:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [
-      supabaseURL + "nlsd/" + "nlsd_chicago.gif",
-      supabaseURL + "nlsd/" + "nlsd_chicago_b.webp",
+    images: [supabaseURL + "spas/" + "spas_video.mp4"],
+    tools: ["react-router", "redux", "tailwindcss"],
+  },
+  {
+    name: "stylinglib",
+    category: "web-dev",
+    title: "Styling Management App",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [supabaseURL + "styling-lib/" + "styling_lib_v1_cover.mp4"],
+    tools: [
+      "react",
+      "javascript",
+      "html&css",
+      "express",
+      "mongodb",
+      "cloudinary",
     ],
-    tools: ["urban design", "sketch", "photoshop"],
+  },
+  {
+    name: "xm-portfolio",
+    category: "web-dev",
+    title: "Xiye's Portfolio Website",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [supabaseURL + "xm-portfolio/" + "xm-portfolio.mp4"],
+    tools: ["react", "javascript", "html", "tailwindcss", "supabase"],
   },
   {
     name: "lv",
@@ -41,6 +60,39 @@ const projectData = [
       "illustrator",
       "indesign",
     ],
+  },
+  {
+    name: "aavs-workshop",
+    category: "research-visualization",
+    title: "Growth",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [
+      supabaseURL + "aavs-workshop/" + "aavs_built.webp",
+      supabaseURL + "aavs-workshop/" + "aavs_photo-1.webp",
+    ],
+    tools: ["processing", "keyshot", "rhino", "grasshopper", "VR/AR"],
+  },
+  {
+    name: "graphic-design",
+    category: "graphic-product",
+    title: "Graphic Design",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [supabaseURL + "graphic-product/" + "adp-poster.webp"],
+    tools: ["sketch", "photoshop", "illustrator"],
+  },
+  {
+    name: "nlsd",
+    category: "landscape",
+    title: "North Lake Shore Drive",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [
+      supabaseURL + "nlsd/" + "nlsd_chicago.gif",
+      supabaseURL + "nlsd/" + "nlsd_chicago_b.webp",
+    ],
+    tools: ["urban design", "sketch", "photoshop"],
   },
   {
     name: "playgrounds",
@@ -61,76 +113,6 @@ const projectData = [
     tools: ["procreate", "sketchup", "lumion"],
   },
   {
-    name: "xm-portfolio",
-    category: "web-dev",
-    title: "Xiye's Portfolio Website",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "xm-portfolio/" + "xm-portfolio.mp4"],
-    tools: ["react", "javascript", "html", "tailwindcss", "supabase"],
-  },
-  {
-    name: "identifiers",
-    category: "landscape",
-    title: "Neighborhood Identifiers",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "identifiers/" + "identifiers.gif"],
-    tools: ["rhino", "construction documentation"],
-  },
-  {
-    name: "spas",
-    category: "web-dev",
-    title: "Practice - SPA (single-page apps)",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "spas/" + "spas_video.mp4"],
-    tools: ["react-router", "redux", "tailwindcss"],
-  },
-  {
-    name: "photography",
-    category: "photography",
-    title: "Photography 2016-2023",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "photography/" + "dji_colorado.jpg"],
-    tools: ["iphone", "dji", "gopro", "insta360"],
-  },
-  {
-    name: "designcamp",
-    category: "web-dev",
-    title: "Design Camp",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "designcamp/" + "designcamp_cover.webp"],
-    tools: ["javascript", "html&css", "express", "mongodb", "cloudinary"],
-  },
-  {
-    name: "stylinglib",
-    category: "web-dev",
-    title: "Styling Management App",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "styling-lib/" + "styling_lib_v1_cover.mp4"],
-    tools: [
-      "react",
-      "javascript",
-      "html&css",
-      "express",
-      "mongodb",
-      "cloudinary",
-    ],
-  },
-  {
-    name: "graphic-design",
-    category: "graphic-product",
-    title: "Graphic Design",
-    introduction:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
-    images: [supabaseURL + "graphic-product/" + "adp-poster.webp"],
-    tools: ["sketch", "photoshop", "illustrator"],
-  },
-  {
     name: "la-built",
     category: "landscape",
     title: "Built Landscape Projects",
@@ -143,6 +125,15 @@ const projectData = [
       "indesign",
       "construction documentation",
     ],
+  },
+  {
+    name: "identifiers",
+    category: "landscape",
+    title: "Neighborhood Identifiers",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [supabaseURL + "identifiers/" + "identifiers.gif"],
+    tools: ["rhino", "construction documentation"],
   },
   {
     name: "logo-design",
@@ -161,6 +152,15 @@ const projectData = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
     images: [supabaseURL + "art/" + "oil_painting_1.webp"],
     tools: ["oil-painting", "sketch", "watercolor"],
+  },
+  {
+    name: "photography",
+    category: "photography",
+    title: "Photography 2016-2023",
+    introduction:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maiores!",
+    images: [supabaseURL + "photography/" + "dji_colorado.jpg"],
+    tools: ["iphone", "dji", "gopro", "insta360"],
   },
 ];
 
