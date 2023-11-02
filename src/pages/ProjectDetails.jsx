769602d338175d7c787318e362c04eecf1e206b9
newredskin <xiyemou@gmail.com>
2023-11-01 22:59:19 -0500

    minor fix

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -15,7 +15,7 @@ function ProjectDetails() {
   return (
     <>
       <div className="w-screen min-h-screen bg-orange-50/90 dark:bg-gray-800">
-        <div className="text-teal-900 dark:text-stone-300 mx-16 md:mx-28 lg:mx-56 xl:mx-64 p-1 flex flex-col justify-center items-center">
+        <div className="text-teal-900 dark:text-stone-300 mx-8 sm:mx-14  md:mx-28 lg:mx-56 xl:mx-64 p-1 flex flex-col justify-center items-center">
           <div className="pb-6 lg:max-w-[1536px] max-w-full">
             <h1 className="font-bold text-3xl mb-12 pt-28">{project.title}</h1>
 
