82dcf0ca70942a157a7410f53b1a5060a40dde15
newredskin <xiyemou@gmail.com>
2023-11-01 23:10:47 -0500

    unify page x margin

--- src/features/about/Paragraph.jsx
+++ src/features/about/Paragraph.jsx
@@ -3,7 +3,7 @@ function Paragraph() {
     <div style={{ fontFamily: "Cutive Mono" }}>
       Now, I&apos;m on a new path to become a full-stack developer, and I&apos;m
       bringing with me a wealth of design and{" "}
-      <span className="text-red-700 bg-orange-50/50 underline decoration-2 underline-offset-4">
+      <span className="text-red-700 bg-orange-50/50 dark:bg-orange-50/80 underline decoration-2 underline-offset-4">
         problem-solving
       </span>{" "}
       skills. My goal is to create not only user-friendly websites but also
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -60,7 +60,7 @@ function About() {
   return (
     <>
       <div className="w-screen min-h-screen bg-orange-50/90 dark:bg-gray-800">
-        <div className="text-teal-900 dark:text-stone-300 mx-16 md:mx-24 lg:mx-36 flex flex-col justify-center items-center">
+        <div className="text-teal-900 dark:text-stone-300 mx-8 sm:mx-14 md:mx-28 lg:mx-56 xl:mx-64 flex flex-col justify-center items-center">
           <h1 className="font-bold text-4xl mb-10 pt-28">
             I&apos;m <span className="text-red-700">Xiye</span>.
           </h1>
