ef6149f39cda91b771ccf977871291e93b705794
newredskin <xiyemou@gmail.com>
2023-11-14 22:41:07 -0600

    turn on production switch

--- src/devSwitch.js
+++ src/devSwitch.js
@@ -2,11 +2,11 @@ let imageURL;
 let mediaPrefix;
 
 // Dev Mode
-imageURL = "assets/projects/";
-mediaPrefix = "../";
+// imageURL = "assets/projects/";
+// mediaPrefix = "../";
 
 // Production Mode
-// imageURL = "https://xm-portfolio.s3.us-east-2.amazonaws.com/projects/";
-// mediaPrefix = "";
+imageURL = "https://xm-portfolio.s3.us-east-2.amazonaws.com/projects/";
+mediaPrefix = "";
 
 export { imageURL, mediaPrefix };
